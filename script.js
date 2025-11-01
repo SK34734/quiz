0d0e80d67321c82760e271ebf64cac11505a5382
SK34734 <khanshanzeh.09@gmail.com>
2025-10-31 21:08:13 -0700

    initial file

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 var universalScore = 0;
 var disneyScore = 0;
 var result = document.getElementById("result");
-
 var questionCount = 0;
-/*Add your JavaScript here*/
+
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
 var q2a1 = document.getElementById("q2a1");
 var q2a2 = document.getElementById("q2a2");
 var q3a1 = document.getElementById("q3a1");
-var q3a2 = document.getElementById("q3a2")
+var q3a2 = document.getElementById("q3a2");
 
 function Universal() {
     universalScore += 1;
     questionCount += 1;
-    console.log("questionCount = " + questionCount + "universalScore = " + disneyScore);
+    console.log("questionCount = " + questionCount + " universalScore = " + universalScore);
 
     if (questionCount >= 3) {
-        console.log("The quiz is done!")
         updateResult();
     }
 }
@@ -25,34 +23,25 @@ function Universal() {
 function Disney() {
     disneyScore += 1;
     questionCount += 1;
-    console.log("questionCount = " + questionCount + "disneyScore = " + disneyScore);
+    console.log("questionCount = " + questionCount + " disneyScore = " + disneyScore);
 
     if (questionCount >= 3) {
-        console.log("The quiz is done!")
         updateResult();
     }
 }
-
-var restart = document.getElementById("restart");
-
-//event listeners
-q1a1.addEventListener("click", Universal);
-q1a2.addEventListener("click", Disney);
-q2a1.addEventListener("click", Disney);
-q2a2.addEventListener("click", Universal);
-q3a1.addEventListener("click", Disney);
-q3a2.addEventListener("click", Universal);
-restart.addEventListener("click", restartQuiz);
 
 function updateResult() {
     if (universalScore >= 2) {
         result.innerHTML = "You should head to Universal Studios!";
         console.log("You should head to Universal Studios!");
     } else if (disneyScore >= 2) {
-        result.innerHTML = "You should head to Disneyland!"
-        console.log("You should head to Universal Studios!");
+        result.innerHTML = "You should head to Disneyland!";
+        console.log("You should head to Disneyland!");
     }
 }
+
+var restart = document.getElementById("restart");
+restart.addEventListener("click", restartQuiz);
 
 function restartQuiz() {
     result.innerHTML = "Your result is...";
@@ -60,6 +49,13 @@ function restartQuiz() {
     universalScore = 0;
     disneyScore = 0;
 }
+
+q1a1.addEventListener("click", Universal);
+q1a2.addEventListener("click", Disney);
+q2a1.addEventListener("click", Disney);
+q2a2.addEventListener("click", Universal);
+q3a1.addEventListener("click", Disney);
+q3a2.addEventListener("click", Universal);
 
 const popup = document.getElementById("popup");
 const openBtn = document.getElementById("open-popup");
@@ -72,5 +68,3 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     popup.classList.add("hidden");
 });
-
-
